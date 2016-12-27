3665b9a2dc64920ed1862d2796dd7a27ea803fcc
Aniruddha Das <Aniruddha.Das@altimetrik.com>
2016-12-27 18:30:13 +0530

    Added Files

--- Classes/ValidateEmail.swift
+++ Classes/ValidateEmail.swift
@@ -10,7 +10,7 @@ import Foundation
 
 public extension String {
     
-    func validateEmail() -> Bool {
+    public func validateEmail() -> Bool {
         let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
         let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
         return predicate.evaluate(with: self)
