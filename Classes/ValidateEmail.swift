fcead9da77506a56320ba54c19219b863aa45e17
Aniruddha Das <Aniruddha.Das@altimetrik.com>
2016-12-27 18:21:23 +0530

    Added Files

---
+++ Classes/ValidateEmail.swift
@@ -0,0 +1,19 @@
+//
+//  ValidateEmail.swift
+//  AKStringHelper
+//
+//  Created by Aniruddha Das on 12/27/16.
+//  Copyright © 2016 Aniruddha Das. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    
+    func validateEmail() -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return predicate.evaluate(with: self)
+    }
+    
+}
